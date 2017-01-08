6c1182563f6656069d9ada233709ba63bf4b7d18
梁健 <leungjain@qq.com>
2017-01-09 00:04:38 +0800

    上传了抢红包程序

---
+++ qianghongbao-master/app/src/main/java/com/codeboy/qianghongbao/widget/login.java
@@ -0,0 +1,16 @@
+package com.codeboy.qianghongbao.widget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017-01-08.
+ */
+public class login extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
